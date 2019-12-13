53aea1d69bcc39c3a121ad22d8c2436fa75a80e3
yu-yama <yu-yama@users.noreply.github.com>
2019-12-12 22:09:52 -0800

    [fix] changed std:: declaration
    Got rid of "namespace std" declaration overall, and added "std::" for each element in matrixIntArray.h
    Resolved linker error

--- matrixIntArray/matrixIntArray.h
+++ matrixIntArray/matrixIntArray.h
@@ -7,68 +7,66 @@
 #include <iostream>
 #include <vector>
 
-namespace std {
-    class Matrix {
-    private:
-        int n, m;
-        int* mat;
+class Matrix {
+private:
+    int n, m;
+    int* mat;
 
-    public:
-        explicit Matrix(int n, int m);
-        explicit Matrix(vector< vector<int> > matData);
-        explicit Matrix(vector<int> matData, int n);
-        Matrix(const Matrix& p);
+public:
+    explicit Matrix(int n, int m);
+    explicit Matrix(std::vector< std::vector<int> > matData);
+    explicit Matrix(std::vector<int> matData, int n);
+    Matrix(const Matrix& p);
 
-        ~Matrix() {
-            delete[] mat;
-        }
+    ~Matrix() {
+        delete[] mat;
+    }
 
-        int row()    const { return n; }
-        int column() const { return m; }
+    int row()    const { return n; }
+    int column() const { return m; }
 
-        vector<int> at_row   (int r) const;
-        vector<int> at_column(int c) const;
-        int& at(int r, int c);
-        const int& at(int r, int c) const;
+    std::vector<int> at_row   (int r) const;
+    std::vector<int> at_column(int c) const;
+    int& at(int r, int c);
+    const int& at(int r, int c) const;
 
-        void resize(int nn, int mm);
-        Matrix  operator+() const;
-        Matrix  operator-() const;
+    void resize(int nn, int mm);
+    Matrix  operator+() const;
+    Matrix  operator-() const;
 
-        Matrix& operator=(Matrix p);
+    Matrix& operator=(Matrix p);
 
-        Matrix& operator+=(Matrix p);
-        Matrix& operator-=(Matrix p);
-        Matrix& operator*=(Matrix p);
-        Matrix& operator*=(int p);
-        // Matrix& operator/=(Matrix p);
-        Matrix& operator/=(int p);
+    Matrix& operator+=(Matrix p);
+    Matrix& operator-=(Matrix p);
+    Matrix& operator*=(Matrix p);
+    Matrix& operator*=(int p);
+    // Matrix& operator/=(Matrix p);
+    Matrix& operator/=(int p);
 
-        Matrix  operator+(Matrix p) const;
-        Matrix  operator-(Matrix p) const;
-        Matrix  operator*(Matrix p) const;
-        Matrix  operator*(int p) const;
-        friend Matrix operator*(int n, const Matrix& p);
-        // Matrix  operator/(Matrix p) const;
-        Matrix  operator/(int p) const;
-        // friend Matrix operator/(int n, const Matrix& p);
+    Matrix  operator+(Matrix p) const;
+    Matrix  operator-(Matrix p) const;
+    Matrix  operator*(Matrix p) const;
+    Matrix  operator*(int p) const;
+    friend Matrix operator*(int n, const Matrix& p);
+    // Matrix  operator/(Matrix p) const;
+    Matrix  operator/(int p) const;
+    // friend Matrix operator/(int n, const Matrix& p);
 
-        bool operator==(const Matrix& p) const;
-        bool operator!=(const Matrix& p) const;
+    bool operator==(const Matrix& p) const;
+    bool operator!=(const Matrix& p) const;
 
-        Matrix row_echelon() const;
-        // Matrix reduced_row_echelon() const;
-        // int det() const;
-        // Matrix inv() const;
-        // Matrix pow() const;
-        //
-        string to_string() const;
-        // string to_latex() const;
+    Matrix row_echelon() const;
+    // Matrix reduced_row_echelon() const;
+    // int det() const;
+    // Matrix inv() const;
+    // Matrix pow() const;
+    //
+    std::string to_string() const;
+    // std::string to_latex() const;
 
-        friend ostream& operator<<(ostream& s, const Matrix& x);
+    friend std::ostream& operator<<(std::ostream& s, const Matrix& x);
 
-        friend istream& operator>>(istream& s,       Matrix& x);
-    };
-}
+    friend std::istream& operator>>(std::istream& s,       Matrix& x);
+};
 
 #endif
